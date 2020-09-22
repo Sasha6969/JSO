a5a08016242db26fb3402a042ceb012168ad770b
Sasha6969 <71279648+Sasha6969@users.noreply.github.com>
2020-09-22 15:29:55 +0200

    start

--- src/WS2/Main.java
+++ src/WS2/Main.java
@@ -4,22 +4,16 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
+
+		System.out.println("\t\t-----MENU-----");
+		System.out.println("1. Create");
+		System.out.println("2. Retrieve");
+		System.out.println("3. Update");
+		System.out.println("4. Delete");
+		System.out.println("5. Help");
 		
-		System.out.print("Enter Name: ");
-		String name = sc.nextLine();
-		
-		System.out.print("Enter PN(YYMMDDXXXX): ");
-		long pn = sc.nextLong();
-		
-		int uid = 0001;
-		
-		Member one = new Member(name, uid, pn);
-		
-		System.out.println("Hi "+one.getName());
-		System.out.println("Your PN is "+one.getPN());
-		System.out.println("Your UID is "+one.getUID());
 		
 	}
 }
