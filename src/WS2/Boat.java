bebd433d5898b62a4145003a141d9855aeab8b04
Sasha6969 <71279648+Sasha6969@users.noreply.github.com>
2020-09-22 12:25:59 +0200

    First Changes from SJ
    getters and setter of boat and members. object construction

--- src/WS2/Boat.java
+++ src/WS2/Boat.java
@@ -1,4 +1,28 @@
 package WS2;
 
 public class Boat {
+	
+	private int boatLength;
+	private String boatType;
+	
+	
+	public int getBoatLength() {
+		return boatLength;
+	}
+	public void setBoatLength(int boatLength) {
+		this.boatLength = boatLength;
+	}
+	public String getBoatType() {
+		return boatType;
+	}
+	public void setBoatType(String boatType) {
+		this.boatType = boatType;
+	}
+	
+	public Boat() {}
+	
+	public Boat(int boatLength, String boatType) {
+		boatLength = this.boatLength;
+		boatType = this.boatType;
+	}
 }
