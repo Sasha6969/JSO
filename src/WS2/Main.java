8b96d7baa519903751f2320b3f823a897992a17c
Sasha6969 <71279648+Sasha6969@users.noreply.github.com>
2020-09-22 12:28:35 +0200

    First Initial Changes by SJ

--- src/WS2/Main.java
+++ src/WS2/Main.java
@@ -1,4 +1,8 @@
 package WS2;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
 }
--- src/WS2/Member.java
+++ src/WS2/Member.java
@@ -1,4 +1,30 @@
 package WS2;
 
 public class Member {
+	
+	private int UID;
+	private long PN;
+	private String name;
+	
+	public int getUID() {
+		return UID;
+	}
+	public void setUID(int uID) {
+		UID = uID;
+	}
+	public long getPN() {
+		return PN;
+	}
+	public void setPN(long pN) {
+		PN = pN;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
 }
