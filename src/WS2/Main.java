4b5fb6f131d5ce3ca321349d121452cdbb9bc4d1
Sasha6969 <71279648+Sasha6969@users.noreply.github.com>
2020-09-22 16:34:14 +0200

    hellooo

--- src/WS2/Main.java
+++ src/WS2/Main.java
@@ -4,16 +4,66 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-
+		
+		
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("\t\t-----MENU-----");
+		int uid = 0000;
+		
+		System.out.println("\t   Welcome to the Yacht club");
+		System.out.println("\t\t-----MENU-----\n");
+		System.out.println("Choose an option from below");
 		System.out.println("1. Create");
 		System.out.println("2. Retrieve");
 		System.out.println("3. Update");
 		System.out.println("4. Delete");
-		System.out.println("5. Help");
+		System.out.println("5. Help\n");
+		System.out.print("Enter a number: ");
 		
+		int menuChoice = sc.nextInt();
 		
+		boolean invalidInput=false;
+		while(!invalidInput) {
+		
+		try {
+
+		switch (menuChoice) {
+		
+		case 1:
+			
+			Scanner scan = new Scanner(System.in);
+			String name;
+			int persNum;
+			
+			System.out.println("\n\t--Create a member--");
+			
+			System.out.print("Enter the Full Name: ");
+			name = scan.nextLine();		
+			
+			System.out.print("Enter the Personal Number (YYMMDDXXXX): ");
+			persNum = scan.nextInt();
+			
+			Member one = new Member(name, uid, persNum);
+			System.out.println("Member "+one.getName()+" has been created");
+			
+			uid++;
+			break;
+		case 2:
+			System.out.println("you entered 2");
+			break;
+		case 3:
+			System.out.println("you entered 3");
+			break;
+		case 4:
+			System.out.println("you entered 4");
+			break;
+		case 5:
+			System.out.println("you entered 5");
+			break;
+		}
+		}catch(Exception es) {
+			System.out.print("Enter a Valid Number: ");
+			menuChoice = sc.nextInt();
+		}
+		}
 	}
 }
